8e57e061ea6489d581c5b898a816b1a76387da56
Pedro <rpedroricardo@gmail.com>
2026-05-05 17:14:11 -0300

    feat: starting models of veiculos api

--- backend/veiculos/logica/models.py
+++ backend/veiculos/logica/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Veiculo(models.Model):
+    nome = models.CharField()
+    tipo = models.CharField(choices='Carro, Moto'.strip(","))
